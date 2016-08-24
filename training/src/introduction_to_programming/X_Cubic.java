ad3f5b8492758af49f29671e03dd60b457768711
inotakuya <i_no_u_u@yahoo.co.jp>
2016-08-25 00:58:48 +0900

    3

--- training/src/introduction_to_programming/X_Cubic.java
+++ training/src/introduction_to_programming/X_Cubic.java
@@ -1,7 +1,5 @@
 package introduction_to_programming;
 
-import java.util.Scanner;
-
 /*x の 3 乗
 
 １つの整数 x を読み込んで、x の３乗を計算し結果を出力するプログラムを作成して下さい。
@@ -24,6 +22,8 @@ Sample Input 2
 3
 Sample Output 2
 27*/
+
+import java.util.Scanner;
 
 public class X_Cubic {
 	public static void main(String[] args) {
