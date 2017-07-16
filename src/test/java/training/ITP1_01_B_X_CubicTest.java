a29ee943176953c9e3c6cf8b81a23ab647258913
inotakuya <i_no_u_u@yahoo.co.jp>
2017-07-16 14:16:14 +0900

    テスト追加

--- src/test/java/training/ITP1_01_B_X_CubicTest.java
+++ src/test/java/training/ITP1_01_B_X_CubicTest.java
@@ -1,44 +1,48 @@
 package training;
 
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import java.io.BufferedOutputStream;
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 import java.io.PrintStream;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import introduction_to_programming.ITP1_01_A_HelloWorld;
+import introduction_to_programming.ITP1_01_B_X_Cubic;
 
-public class ITP1_01_A_HelloWorldTest {
+public class ITP1_01_B_X_CubicTest {
 	
-	private PrintStream defaultOutPrintStream;
+	private PrintStream defaultPrintStream;
 	private ByteArrayOutputStream byteArrayOutputStream; 
+	private StandardInputSnatcher in = new StandardInputSnatcher();
 
 	@Before
 	public void setUp(){
-		defaultOutPrintStream = System.out;
+		defaultPrintStream = System.out;
 		byteArrayOutputStream = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(new BufferedOutputStream(byteArrayOutputStream)));
+		System.setIn(in);
 	}
+	
+	@After
+	public void tearDown(){
+		System.setOut(defaultPrintStream);
+		System.setIn(System.in);
+	}
+
 	@Test
 	public void testMain() {
-		ITP1_01_A_HelloWorld.main(new String[]{});
+		in.inputln("3");
+		ITP1_01_B_X_Cubic.main(null);
 		System.out.flush();
 		final String actual = byteArrayOutputStream.toString();
 		
-		final String expected = "Hello World" + System.lineSeparator();
+		final String expected = "27" + System.lineSeparator();
 		
 		assertThat(actual, is(expected));
+	}
 
-	}
-	@After
-	public void tearDown(){
-		System.setOut(defaultOutPrintStream);
-	}
 }
